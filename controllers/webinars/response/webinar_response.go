51f27d4e6f3f56c94c72aa229592dadcce541e02
Zanuardi Novanda <zanuardinovanda@gmail.com>
2021-08-18 21:46:38 +0700

    fixing filter webinar

--- controllers/webinars/response/webinar_response.go
+++ controllers/webinars/response/webinar_response.go
@@ -9,18 +9,19 @@ import (
 )
 
 type Webinars struct {
-	ID          int               `json:"id"`
-	UserID      int               `json:"user_id"`
-	CategoryID  int               `json:"category_id"`
-	Categories  response.Category `json:"categories"`
-	ImageUrl    string            `json:"image_url"`
-	Name        string            `json:"name"`
-	Description string            `json:"description"`
-	Price       float64           `json:"price"`
-	Schedule    time.Time         `json:"schedule"`
-	CreatedAt   time.Time         `json:"created_at"`
-	UpdatedAt   time.Time         `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt    `json:"deleted_at"`
+	ID           int               `json:"id"`
+	UserID       int               `json:"user_id"`
+	CategoryID   int               `json:"category_id"`
+	Categories   response.Category `json:"categories"`
+	CategoryName string            `json:"category_name"`
+	ImageUrl     string            `json:"image_url"`
+	Name         string            `json:"name"`
+	Description  string            `json:"description"`
+	Price        float64           `json:"price"`
+	Schedule     time.Time         `json:"schedule"`
+	CreatedAt    time.Time         `json:"created_at"`
+	UpdatedAt    time.Time         `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt    `json:"deleted_at"`
 }
 
 func FromDomain(domain webinars.Domain) Webinars {
